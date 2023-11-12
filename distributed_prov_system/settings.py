1195ff8ba51e3f3aa9b15b3854e0b32cbbae40b1
David Gajdos <david.gajdos97@gmail.com>
2023-11-12 18:50:18 +0100

    Add django_neomodel dependency

--- distributed_prov_system/settings.py
+++ distributed_prov_system/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+NEOMODEL_NEO4J_BOLT_URL = os.environ.get('NEO4J_BOLT_URL', 'bolt://neo4j:password@localhost:7687')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_neomodel'
 ]
 
 MIDDLEWARE = [
