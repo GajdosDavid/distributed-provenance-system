faf471aa8637e28a9eeb48cd80c9c4e30d05bb21
David Gajdos <david.gajdos97@gmail.com>
2023-11-12 21:32:53 +0100

    Define models for neo4j database

--- provenance/models.py
+++ provenance/models.py
@@ -1,3 +1,59 @@
-from django.db import models
+from neomodel import (StructuredRel, StructuredNode, UniqueIdProperty,
+                      StringProperty, JSONProperty, DateTimeFormatProperty, RelationshipTo, RelationshipFrom)
 
-# Create your models here.
+
+class BaseProvRelationship(StructuredRel):
+    identifier = StringProperty()
+    attributes = JSONProperty()
+
+
+class WasGeneratedBy(BaseProvRelationship):
+    time = DateTimeFormatProperty()
+
+
+class Usage(BaseProvRelationship):
+    time = DateTimeFormatProperty()
+
+
+class WasInformedBy(BaseProvRelationship):
+    pass
+
+class WasStartedBy(BaseProvRelationship):
+
+
+
+class BundledIn(BaseProvRelationship):
+    time = DateTimeFormatProperty()
+
+
+class BaseProvNode(StructuredNode):
+    uid = UniqueIdProperty()
+    identifier = StringProperty(required=True)
+    attributes = JSONProperty()
+
+    bundled_in = RelationshipTo('Bundle', 'BUNDLED_IN', model=BundledIn)
+
+
+class Entity(BaseProvNode):
+    was_generated_by = RelationshipTo('Activity', 'WAS_GENERATED_BY', model=WasGeneratedBy)
+
+    used = RelationshipFrom('Activity', 'USED', model=Usage)
+
+
+class Activity(BaseProvNode):
+    start_time = DateTimeFormatProperty()
+    end_time = DateTimeFormatProperty()
+
+    used = RelationshipTo('Entity', 'USED', model=Usage)
+    was_informed_by = RelationshipTo('Activity', 'WAS_INFORMED_BY', model=WasGeneratedBy)
+
+    was_generated_by = RelationshipFrom('Entity', 'WAS_GENERATED_BY', model=WasGeneratedBy)
+
+
+
+class Agent(BaseProvNode):
+    pass
+
+
+class Bundle(BaseProvNode):
+    pass
