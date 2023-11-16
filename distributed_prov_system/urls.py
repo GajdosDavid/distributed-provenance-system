e5afd8ca400ff9aa3555e1441c7532a9ed3c739d
David Gajdos <david.gajdos97@gmail.com>
2023-11-16 18:43:47 +0100

    Change /app endpoint to /api endpoint

--- distributed_prov_system/urls.py
+++ distributed_prov_system/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('app/', include("provenance.urls")),
+    path('api/', include("provenance.urls")),
     path('admin/', admin.site.urls),
 ]
