6ec7e63d6796bc28b3a0810038db91f2c904d480
David Gajdos <david.gajdos97@gmail.com>
2023-11-24 14:53:16 +0100

    Add module for graph importing into from prov to neo4j

---
+++ prov2neomodel/prov2neomodel.py
@@ -0,0 +1,38 @@
+from prov.model import ProvDocument, ProvElement, ProvRelation
+from .mappers import prov2neo_mappers
+from provenance.models import Bundle
+
+
+def import_graph(document: ProvDocument):
+    assert document.has_bundles(), 'No bundles inside the document!'
+
+    for bundle in document.bundles:
+        models = {}
+
+        neo_bundle = Bundle()
+        neo_bundle.identifier = bundle.identifier
+        neo_bundle.save()
+
+        # Generate nodes first
+        for prov_elem in bundle.get_records(ProvElement):
+            mapper_class = prov2neo_mappers.get(type(prov_elem))
+            mapper = mapper_class(prov_elem)
+            mapper.do_mapping()
+
+            model = mapper.get_neomodel()
+            model.save()
+            model.bundled_in.connect(neo_bundle)
+
+            assert model.identifier not in models
+            models[model.identifier] = model
+
+        # Now connect the nodes
+        for prov_relation in bundle.get_records(ProvRelation):
+            print(prov_relation)
+
+        # TODO -- do this only within a scope of bundle to avoid doing it for all nodes
+        # query = ("MATCH (node) "
+        #          "WHERE node.attributes IS NOT NULL "
+        #          "SET node += apoc.convert.fromJsonMap(node.attributes) "
+        #          "REMOVE node.attributes")
+        # results, meta = db.cypher_query(query, None, resolve_objects=True)
