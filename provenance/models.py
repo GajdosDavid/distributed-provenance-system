f0fcf99650d101862291f0ef9694d66197c080e7
David Gajdos <david.gajdos97@gmail.com>
2023-11-26 18:01:59 +0100

    Remove relationship nodes which are replaced by Fake* nodes

--- provenance/models.py
+++ provenance/models.py
@@ -1,116 +1,79 @@
-from neomodel import (StructuredRel, StructuredNode, UniqueIdProperty,
-                      StringProperty, JSONProperty, DateTimeFormatProperty, RelationshipTo, RelationshipFrom,
-                      cardinality)
-
-'''
-class BundledIn(StructuredRel):
-    identifier = StringProperty()
-    attributes = JSONProperty()
-'''
+from neomodel import (StructuredRel, StructuredNode, StringProperty,
+                      JSONProperty, DateTimeFormatProperty, RelationshipTo)
 
 
-### Classes representing prov-dm relations between PROV-DM types (require both nodes) ###
+# Classes representing relationships between individual nodes that can hold additional information
 class BaseProvRel(StructuredRel):
     identifier = StringProperty()
     attributes = JSONProperty()
 
 
-class WasGeneratedByRel(BaseProvRel):
+class WasGeneratedBy(BaseProvRel):
     time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
 
 
-class UsedRel(BaseProvRel):
+class Used(BaseProvRel):
     time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
 
 
-class WasInvalidatedByRel(BaseProvRel):
+class WasInvalidatedBy(BaseProvRel):
     time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
 
 
-class WasDerivedFromRel(BaseProvRel):
+class WasDerivedFrom(BaseProvRel):
     activity = StringProperty()
     generation = StringProperty()
     usage = StringProperty()
 
 
-class WasInformedByRel(BaseProvRel):
+class WasInformedBy(BaseProvRel):
     pass
 
 
-class WasAttributedToRel(BaseProvRel):
+class WasStartedBy(BaseProvRel):
+    starter = StringProperty()
+    time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
+
+
+class WasEndedBy(BaseProvRel):
+    ender = StringProperty()
+    time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
+
+
+class WasAttributedTo(BaseProvRel):
     pass
 
 
-class WasAssociatedWithRel(BaseProvRel):
+class WasAssociatedWith(BaseProvRel):
     plan = StringProperty()
 
 
-class ActedOnBehalfOfRel(BaseProvRel):
+class ActedOnBehalfOf(BaseProvRel):
     activity = StringProperty()
 
 
-class WasInfluencedByRel(BaseProvRel):
+class WasInfluencedBy(BaseProvRel):
     pass
 
 
-### Classes representing PROV-DM relations, however are modeled as neo4j nodes because they allow absence of one of the PROV-DM types ###
-class BaseProvRelNode(StructuredNode):
-    # avoids giving a tag 'BaseProvRelNode' in neo4j
+# Fake nodes which are used when opposite node is not present in graph
+class BaseFakeProvClass(StructuredNode):
+    # avoids giving a tag 'BaseFakeProvClass' in neo4j
     __abstract_node__ = True
-
-    identifier = StringProperty()
-    attributes = JSONProperty()
 
     bundled_in = RelationshipTo('Bundle', 'bundled_in')
 
 
-class WasGeneratedBy(BaseProvRelNode):
-    entity = RelationshipFrom('Entity', 'was_generated_by', cardinality.One)
-
-    activity = RelationshipTo('Activity', 'was_generated_by', cardinality.ZeroOrOne)
-
-    time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
+class FakeActivity(BaseFakeProvClass):
+    pass
 
 
-class Used(BaseProvRelNode):
-    activity = RelationshipFrom('Activity', 'used', cardinality.One)
-
-    entity = RelationshipTo('Entity', 'used', cardinality.ZeroOrOne)
-
-    time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
+class FakeAgent(BaseFakeProvClass):
+    pass
 
 
-class WasStartedBy(BaseProvRelNode):
-    activity = RelationshipFrom('Activity', 'was_started_by', cardinality.One)
-
-    trigger = RelationshipTo('Entity', 'was_started_by', cardinality.ZeroOrOne)
-    starter = RelationshipTo('Activity', 'was_started_by', cardinality.ZeroOrOne)
-
-    time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
-
-
-class WasEndedBy(BaseProvRelNode):
-    activity = RelationshipFrom('Activity', 'was_ended_by', cardinality.One)
-
-    trigger = RelationshipTo('Entity', 'was_ended_by', cardinality.ZeroOrOne)
-    ender = RelationshipTo('Activity', 'was_ended_by', cardinality.ZeroOrOne)
-
-    time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
-
-
-class WasInvalidatedBy(BaseProvRelNode):
-    entity = RelationshipFrom('Entity', 'was_invalidated_by', cardinality.One)
-
-    activity = RelationshipTo('Activity', 'was_invalidated_by', cardinality.ZeroOrOne)
-
-    time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
-
-
-class WasAssociatedWith(BaseProvRelNode):
-    activity = RelationshipFrom('Activity', 'was_associated_with', cardinality.One)
-
-    agent = RelationshipTo('Agent', 'was_associated_with', cardinality.ZeroOrOne)
-    plan = RelationshipTo('Entity', 'was_associated_with', cardinality.ZeroOrOne)
+class FakeEntity(BaseFakeProvClass):
+    pass
 
 
 ### Classes for main PROV-DM types ###
@@ -124,14 +87,19 @@ class BaseProvClass(StructuredNode):
     bundled_in = RelationshipTo('Bundle', 'bundled_in')
 
     # TODO -- might not be correct and might be necessary to move this somewhere else as the relationship can be between all PROV-DM types
-    was_influenced_by = RelationshipTo('BaseProvClass', 'was_influenced_by', model=WasInfluencedByRel)
+    was_influenced_by = RelationshipTo('BaseProvClass', 'was_influenced_by', model=WasInfluencedBy)
 
 
 class Entity(BaseProvClass):
-    was_derived_from = RelationshipTo('Entity', 'was_derived_from', model=WasDerivedFromRel)
-    was_generated_by = RelationshipTo('Activity', 'was_generated_by', model=WasGeneratedByRel)
-    was_invalidated_by = RelationshipTo('Activity', 'was_invalidated_by', model=WasInvalidatedByRel)
-    was_attributed_to = RelationshipTo('Agent', 'was_attributed_to', model=WasAttributedToRel)
+    was_generated_by = RelationshipTo('Activity', 'was_generated_by', model=WasGeneratedBy)
+    was_generated_by_fake = RelationshipTo('FakeActivity', 'was_generated_by', model=WasGeneratedBy)
+
+    was_derived_from = RelationshipTo('Entity', 'was_derived_from', model=WasDerivedFrom)
+
+    was_invalidated_by = RelationshipTo('Activity', 'was_invalidated_by', model=WasInvalidatedBy)
+    was_invalidated_by_fake = RelationshipTo('FakeActivity', 'was_invalidated_by', model=WasInvalidatedBy)
+
+    was_attributed_to = RelationshipTo('Agent', 'was_attributed_to', model=WasAttributedTo)
     specialization_of = RelationshipTo('Entity', 'specialization_of')
     alternate_of = RelationshipTo('Entity', 'alternate_of')
 
@@ -140,16 +108,23 @@ class Activity(BaseProvClass):
     start_time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
     end_time = DateTimeFormatProperty(format='%Y-%m-%dT%H:%M:%S')
 
-    was_informed_by = RelationshipTo('Activity', 'was_informed_by', model=WasInformedByRel)
-    used = RelationshipTo('Entity', 'used', model=UsedRel)
-    was_associated_with = RelationshipTo('Agent', 'was_associated_with', model=WasAssociatedWithRel)
+    used = RelationshipTo('Entity', 'used', model=Used)
+    used_fake = RelationshipTo('FakeEntity', 'used', model=Used)
 
-    # was_started_by?
-    # was_ended_by?
+    was_informed_by = RelationshipTo('Activity', 'was_informed_by', model=WasInformedBy)
+
+    was_associated_with = RelationshipTo('Agent', 'was_associated_with', model=WasAssociatedWith)
+    was_associated_with_fake = RelationshipTo('FakeAgent', 'was_associated_with', model=WasAssociatedWith)
+
+    was_started_by = RelationshipTo('Entity', 'was_started_by', model=WasStartedBy)
+    was_started_by_fake = RelationshipTo('FakeEntity', 'was_started_by', model=WasStartedBy)
+
+    was_ended_by = RelationshipTo('Entity', 'was_ended_by', model=WasEndedBy)
+    was_ended_by_fake = RelationshipTo('FakeEntity', 'was_ended_by', model=WasEndedBy)
 
 
 class Agent(BaseProvClass):
-    acted_on_behalf_of = RelationshipTo('Agent', 'acted_on_behalf_of', model=ActedOnBehalfOfRel)
+    acted_on_behalf_of = RelationshipTo('Agent', 'acted_on_behalf_of', model=ActedOnBehalfOf)
 
 
 class Bundle(BaseProvClass):
