3df144672557b65d3498c0e82ffec3ffe415da92
David Gajdos <david.gajdos97@gmail.com>
2023-11-26 21:58:38 +0100

    Implement graph import into index POST processing function

--- provenance/views.py
+++ provenance/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from prov.model import ProvDocument
+
+from prov2neomodel.prov2neomodel import import_graph
 
 
-# Create your views here.
+@csrf_exempt
+@require_POST
 def index(request):
+    graph = ProvDocument.deserialize(content=request.body, format="rdf")
+    import_graph(graph)
+
     return HttpResponse("Hello, mon frere")
