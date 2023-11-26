a87cd71391e2e93943833d113d444011b83dc8c2
David Gajdos <david.gajdos97@gmail.com>
2023-11-26 21:54:37 +0100

    Import of prov relationship implemented into graph importing

--- prov2neomodel/prov2neomodel.py
+++ prov2neomodel/prov2neomodel.py
@@ -17,7 +17,7 @@ def import_graph(document: ProvDocument):
             import_element(neo_bundle, prov_elem, models)
 
         for prov_relation in bundle.get_records(ProvRelation):
-            import_relations(bundle, prov_relation, models)
+            import_relations(neo_bundle, prov_relation, models)
 
         # TODO -- do this only within a scope of bundle to avoid doing it for all nodes
         # query = ("MATCH (node) "
@@ -38,4 +38,6 @@ def import_element(bundle, elem, models: dict):
 
 
 def import_relations(bundle, relation, models: dict):
-    pass
+    mapper_class = prov2neo_mappers.get(type(relation))
+    mapper = mapper_class(bundle, relation, models)
+    mapper.save()
