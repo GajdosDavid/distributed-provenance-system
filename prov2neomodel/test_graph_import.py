316a309b8dc19722e6a782c605717482bac06a3c
David Gajdos <david.gajdos97@gmail.com>
2023-11-27 16:55:49 +0100

    Tests for ProvElement mappers

--- prov2neomodel/test_graph_import.py
+++ prov2neomodel/test_graph_import.py
@@ -1,1 +1,13 @@
-# tests will one day be here, haha
+import unittest
+
+
+class ImportElementTestCase(unittest.TestCase):
+    pass
+
+
+class ImportRelationTestCase(unittest.TestCase):
+    pass
+
+
+class ImportGraphTestCase(unittest.TestCase):
+    pass
---
+++ prov2neomodel/test_mappers.py
@@ -0,0 +1,56 @@
+from unittest import TestCase, mock
+from . import mappers
+from datetime import datetime
+from prov.model import ProvEntity, ProvBundle, ProvAgent, ProvActivity
+
+
+class ElementMappersTestCase(TestCase):
+    _bundle = ProvBundle(identifier='Bundle1', namespaces={"ex": "http://example.com/"})
+
+    def test_entity_import(self):
+        entity_id = 'entity1'
+        attributes = {'prov:type': 'document',
+                      'ex:version': '2',
+                      'ex:foo': 'bar'}
+        prov_entity = ProvEntity(self._bundle, entity_id, attributes)
+        mapper = mappers.ProvEntityMapper(None, prov_entity)
+        mapper.map_to_neomodel()
+
+        neomodel = mapper.get_neomodel()
+        self.assertEqual(neomodel.identifier, entity_id)
+        self.assertEqual(neomodel.attributes, attributes)
+
+    def test_agent_import(self):
+        agent_id = 'agent1'
+        attributes = {'ex:employee': '1234',
+                      'ex:name': 'Alice',
+                      'prov:type': 'prov:Person'}
+        prov_agent = ProvAgent(self._bundle, agent_id, attributes)
+        mapper = mappers.ProvAgentMapper(None, prov_agent)
+        mapper.map_to_neomodel()
+
+        neomodel = mapper.get_neomodel()
+        self.assertEqual(neomodel.identifier, agent_id)
+        self.assertEqual(neomodel.attributes, attributes)
+
+    def test_activity_import(self):
+        activity_id = 'activity1'
+        attributes = {'ex:employee': '1234',
+                      'ex:host': "server.example.org",
+                      'prov:type': 'ex:edit'}
+        prov_activity = ProvActivity(self._bundle, activity_id, attributes)
+        start = datetime(2023, 2, 2, 22, 22, 22)
+        end = datetime(2024, 1, 2, 3, 4, 5)
+        prov_activity.set_time(start, end)
+        mapper = mappers.ProvActivityMapper(None, prov_activity)
+        mapper.map_to_neomodel()
+
+        neomodel = mapper.get_neomodel()
+        self.assertEqual(neomodel.identifier, activity_id)
+        self.assertEqual(neomodel.start_time, start)
+        self.assertEqual(neomodel.end_time, end)
+        self.assertEqual(neomodel.attributes, attributes)
+
+
+class RelationMappersTestCase(TestCase):
+    pass
