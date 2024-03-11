eec3a66367a521bc403581973d5dae965178eea7
David Gajdos <david.gajdos97@gmail.com>
2024-03-11 18:36:11 +0100

    DPS: Remove unnecessary code for provenance graph serizalization

--- distributed_prov_system/prov2neomodel/prov2neomodel.py
+++ distributed_prov_system/prov2neomodel/prov2neomodel.py
@@ -1,7 +1,5 @@
-import uuid
 from datetime import datetime
-from prov.model import ProvDocument, ProvElement, ProvRelation, ProvActivity
-from .mappers import prov2neo_mappers
+from prov.model import ProvDocument, ProvActivity
 from provenance.models import Bundle, Entity, Document, Activity, Agent
 from neomodel.exceptions import DoesNotExist
 from neomodel.match import Traversal, OUTGOING
@@ -138,49 +136,3 @@ def get_main_activity_id(bundle):
                 return activity.identifier.localpart
 
     return None
-
-
-# leaving this here if some time in future it'd be necessary to split document into individual nodes
-# def __import_graph__(document: ProvDocument, json_data):
-#     assert document.has_bundles(), 'No bundles inside the document!'
-#
-#     for bundle in document.bundles:
-#         models = {}
-#
-#         neo_bundle = Bundle()
-#         neo_bundle.identifier = str(bundle.identifier) + '_v1'
-#         neo_bundle.graph = json_data['graph']['data']
-#         neo_bundle.signature = json_data['signature']
-#         neo_bundle.timestamp = datetime.fromtimestamp(json_data['timestamp'])
-#         neo_bundle.save()
-#
-#         # create_and_import_meta_provenance(str(bundle.identifier), json_data)
-#
-#         for prov_elem in bundle.get_records(ProvElement):
-#             import_element(neo_bundle, prov_elem, models)
-#
-#         for prov_relation in bundle.get_records(ProvRelation):
-#             import_relation(neo_bundle, prov_relation, models)
-#
-#         # TODO -- do this only within a scope of bundle to avoid doing it for all nodes
-#         # query = ("MATCH (node) "
-#         #          "WHERE node.attributes IS NOT NULL "
-#         #          "SET node += apoc.convert.fromJsonMap(node.attributes) "
-#         #          "REMOVE node.attributes")
-#         # results, meta = db.cypher_query(query, None, resolve_objects=True)
-#
-#
-# def import_element(bundle, elem, models: dict):
-#     mapper_class = prov2neo_mappers.get(type(elem))
-#     mapper = mapper_class(bundle, elem)
-#     mapper.save()
-#
-#     model = mapper.get_neomodel()
-#     assert model.identifier not in models
-#     models[model.identifier] = model
-#
-#
-# def import_relation(bundle, relation, models: dict):
-#     mapper_class = prov2neo_mappers.get(type(relation))
-#     mapper = mapper_class(bundle, relation, models)
-#     mapper.save()
